3f67559516b7965dac9e34dce640f22a73426e51
Estoxoxo <155571524+Estoxoxo@users.noreply.github.com>
2024-08-12 17:30:53 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configura el entorno de Streamlit
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@example.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = \$PORT\n\
+" > ~/.streamlit/config.toml
